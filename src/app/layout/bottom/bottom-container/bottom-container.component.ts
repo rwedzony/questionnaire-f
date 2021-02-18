b571cf7c054006b4e0597c5480a7568f675ac9b2
rwedzony <rwedzony@gmail.com>
2021-02-18 21:01:37 +0100

    prepared complete querstionaire design

--- src/app/layout/bottom/bottom-container/bottom-container.component.ts
+++ src/app/layout/bottom/bottom-container/bottom-container.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
-interface Food {
-  value: string;
-  viewValue: string;
-}
 
 @Component({
   selector: 'app-bottom-container',
@@ -19,12 +15,16 @@ export class BottomContainerComponent implements OnInit {
   firstFormGroup: FormGroup;
   // @ts-ignore
   secondFormGroup: FormGroup;
+  // @ts-ignore
+  thirdFormGroup: FormGroup;
 
   // tslint:disable-next-line:variable-name
   constructor(private _formBuilder: FormBuilder) {}
 
   // tslint:disable-next-line:typedef
   selected: any;
+  isOptional = true;
+
   ngOnInit() {
     this.firstFormGroup = this._formBuilder.group({
       firstGroupCtrl1: ['', Validators.required],
@@ -33,5 +33,14 @@ export class BottomContainerComponent implements OnInit {
     this.secondFormGroup = this._formBuilder.group({
       secondGroupCtrl: ['', Validators.required]
     });
+    this.thirdFormGroup = this._formBuilder.group({
+      thirdGroupCtrl: ['', Validators.required]
+    });
+
+  }
+  submit(){
+  }
+  submitForm() {
+    alert("button was clicked!");
   }
 }
