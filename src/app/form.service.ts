5e0921d45b4dfce7ee017711ed1f73bf5a60b3f0
rwedzony <rwedzony@gmail.com>
2021-02-19 01:36:01 +0100

    corrected form service

--- src/app/form.service.ts
+++ src/app/form.service.ts
@@ -11,10 +11,6 @@ export class FormService {
     this.baseUrl = 'https://questionnaire-back.herokuapp.com/forms';
   }
   addForm(formDTO: FormDTO) {
-    // this.httpClient.post(this.urlGet,employee).subscribe(
-    //   (value) => {console.log('Received value: ',value)},
-    //   (error) => {console.log('Error!!',error)},
-    //   ()=>{console.log('end of values')});
     return this.httpClient.post(this.baseUrl, formDTO);
   }
 }
